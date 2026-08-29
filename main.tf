1f83540876f1d583a96694de5039c87ef9bff8a7
aulhassan <alizahulhassan@hotmail.com>
2026-08-29 19:32:49 +0100

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraformstatefolder5" {
+  bucket = "terraformstatefolder5"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformstatefolder5"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
